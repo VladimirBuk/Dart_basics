8306e8c51f2b5d9561a8176c474a56b0636f770c
VladimirBuk <140095681+VladimirBuk@users.noreply.github.com>
2023-07-29 20:09:04 +0600

    Delete ReturnNumbers.dart

--- dart_skillbox1/lib/src/ReturnNumbers.dart
+++
@@ -1,7 +0,0 @@
-num returnNumbers(List<String> inputData) {
-  RegExp exp = RegExp(r"[^0-9]+");
-  String str = inputData.toString();
-  String res = str.replaceAll(exp, '');
-  int result = int.parse(res);
-  return result;
-}
